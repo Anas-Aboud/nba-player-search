44210d026aa6e85daaee1289b2e3dcbe8df07dd0
Anas-Aboud <anss1725@gmail.com>
2026-09-08 23:22:09 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  BALLDONTLIE_API_KEY: "14cf7dc7-4c3e-496f-8ed1-df65e2fd848e",
+  BALLDONTLIE_API_KEY: " ",
   // Optional: override the endpoint for local testing.
   API_URL: "https://api.balldontlie.io/v1/players",
 };
